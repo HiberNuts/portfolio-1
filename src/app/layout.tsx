ab89fd9dff3d5bed674eb1bfd5882304eaec9b66
hibernuts <raghavjindal0212@gmail.com>
2026-01-25 12:17:52 +0530

    chore: replace favicon with SVG icon in layout and remove unused Head component from page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
   },
   description: DATA.description,
   icons: {
-    icon: "/favicon.png",
+    icon: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>👾</text></svg>",
   },
 
   openGraph: {
